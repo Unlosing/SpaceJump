36d23fc695eebbc67bdbfd8e321f91ae97fe91fd
Alex M <unlosingplays@gmail.com>
2020-03-15 07:16:59 -0700

    Update Game.cpp

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -18,7 +18,7 @@
  *	You should have received a copy of the GNU General Public License					  *
  *	along with The Chili DirectX Framework.  If not, see <http://www.gnu.org/licenses/>.  *
  ******************************************************************************************/
-// Game Created by Alex Meyer
+// Game Created by Unlosing
 #include "MainWindow.h"
 #include "Game.h"
 
